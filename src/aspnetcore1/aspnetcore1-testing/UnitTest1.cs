2824b45c4835b403774d1ad27dc94dae103d2b2d
udaysingh01 <udaysingh01@nagarro.com>
2024-04-10 13:40:30 +0530

    first commit

---
+++ src/aspnetcore1/aspnetcore1-testing/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aspnetcore1_testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+    }
+}
